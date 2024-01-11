dca520b66f735f2503c4ecd975ab46a68ecdd141
moser297 <90271498+moser297@users.noreply.github.com>
2024-01-11 12:33:34 -0600

    delete from other folder

--- Code2Share_Consciousness_NEO/JM_check_matching_PCA_check.m
+++
@@ -1,9 +0,0 @@
-function [PCA_check_S, PCA_check_D]=JM_check_matching_PCA_check(PCA_check_S, PCA_check_D)
-%function checks, if PCA check was possitive for standard and deviant of a
-%certain file. If it was not for one, the other is also set to 0
-test=any(PCA_check_S,2); 
-test2=any(PCA_check_D,2); 
-mm=any(test-test2, 2);
-PCA_check_S(mm)=0;
-PCA_check_D(mm)=0;
-end
