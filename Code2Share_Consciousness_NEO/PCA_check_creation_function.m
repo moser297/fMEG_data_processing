91fa10c8dd34a2c505dd657ec3884d523825b9b4
moser297 <90271498+moser297@users.noreply.github.com>
2024-01-11 12:33:50 -0600

    delete from other folder

--- Code2Share_Consciousness_NEO/PCA_check_creation_function.m
+++
@@ -1,9 +0,0 @@
-function [PCA_check]=PCA_check_creation_function(sel_comp, Neo_pos_index)
-sel_comp=sel_comp';
-%create count of included datasets
-dat_count=[1:size(Neo_pos_index,2)]';
-%determine positions that need to be filled with zeros
-[A,b]=ismember(dat_count, sel_comp(:,1));
-b(A)=sel_comp(:,2);
-PCA_check=b;
-end
